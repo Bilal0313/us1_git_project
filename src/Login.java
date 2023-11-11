31541112ded44e57a2022d0d636b24c43744ca27
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-10 22:14:18 -0500

    User Class completed

--- src/Login.java
+++ src/Login.java
@@ -2,7 +2,8 @@ public class Login {
 
     public static void main(String[] args) {
         System.out.println("user fields");
-
+        System.out.println("I am dev,working on project");
+        System.out.println("User class completed");
 
     }
 }
